79b9734e3b9332493e9305de61782a7815b39823
OhJihoon <wlgnsdh@coopnc.com>
2017-07-17 18:38:56 +0900

    commit by 2p

---
+++ testGit/src/main/java/com/example/demo/controller/Test01.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Test01 {
+	
+	private String testPrint() {
+		// TODO Auto-generated method stub
+		
+		return "HelloWorld";
+
+	}
+	
+	@RequestMapping("/")
+	public String index() {
+		return "helloworld!";
+	}
+
+}
