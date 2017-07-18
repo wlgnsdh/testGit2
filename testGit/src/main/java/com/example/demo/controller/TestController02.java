2eef0fc4f7404e91fc77c01f6ad60c2832049e0a
OhJihoon <wlgnsdh@coopnc.com>
2017-07-18 13:23:55 +0900

    test b c

--- testGit/src/main/java/com/example/demo/controller/TestController02.java
+++ testGit/src/main/java/com/example/demo/controller/TestController02.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TestController01 {
+public class TestController02 {
 	
 	private boolean isChk = false;
 	
@@ -15,49 +15,49 @@ public class TestController01 {
 	private String testPrint(String gub) {
 		// TODO Auto-generated method stub
 		
-		return "HelloWorld : " + gub + "!!!";
+		return "HelloWorld test4 : " + gub + "!!";
 
 	}
 	
 	
 	
-	@RequestMapping("/index1")
-	public String index1() {
+	@RequestMapping("/indexT1")
+	public String indexT1() {
 		
 		if(!isChk){
 			isChk = true;
 		}
 		
-		gub = "11";
+		gub = "1";
 		
 		return testPrint(gub);
 	}
 	
-	@RequestMapping("/index2")
-	public String index2() {
+	@RequestMapping("/indexT2")
+	public String indexT2() {
 		
 		if(!isChk2){
 			isChk2 = true;
 		}
 		
-		gub = "22";
+		gub = "2";
 		
 		return testPrint(gub);
 	}
 	
-	@RequestMapping("/index3")
-	public String index3() {
+	@RequestMapping("/indexT3")
+	public String indexT3() {
 
 
-		gub = "33";
+		gub = "3";
 		
 		return testPrint(gub);
 	}
 	
-	@RequestMapping("/index4")
-	public String index4() {
+	@RequestMapping("/indexT4")
+	public String indexT4() {
 		
-		gub = "44";
+		gub = "4";
 		
 		return testPrint(gub);
 	}
