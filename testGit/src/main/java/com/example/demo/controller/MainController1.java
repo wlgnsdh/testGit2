6f8fe65cbb902ea0c623c85ef52dd0daa910b58e
OhJihoon <wlgnsdh@coopnc.com>
2017-07-18 15:14:12 +0900

    m c1

---
+++ testGit/src/main/java/com/example/demo/controller/MainController1.java
@@ -0,0 +1,48 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController1 {
+	
+	private boolean isChk = false;
+	
+	private boolean isChk2 = false;
+	
+	private String gub = "";
+	
+	private String testPrint(String gub) {
+		// TODO Auto-generated method stub
+		
+		return "HelloWorld : " + gub + "!!!";
+
+	}
+	
+	
+	
+	@RequestMapping("/main01")
+	public String main01() {
+		
+		if(!isChk){
+			isChk = true;
+		}
+		
+		gub = "main01";
+		
+		return testPrint(gub);
+	}
+	@RequestMapping("/main02")
+	public String main02() {
+		
+		if(!isChk){
+			isChk = true;
+		}
+		
+		gub = "main02";
+		
+		return testPrint(gub);
+	}
+	
+	
+}
