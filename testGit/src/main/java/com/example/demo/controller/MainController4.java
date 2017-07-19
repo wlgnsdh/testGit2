d134a5a62be15e1bea65503bd57fb3eb62a5d5ae
OhJihoon <wlgnsdh@coopnc.com>
2017-07-19 15:08:30 +0900

    b c 1

---
+++ testGit/src/main/java/com/example/demo/controller/MainController4.java
@@ -0,0 +1,37 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.common.CommonUtil;
+
+@RestController
+public class MainController4 {
+	
+	private boolean isChk = false;
+	
+	private boolean isChk2 = false;
+	
+	private String gub = "";
+	
+	private String testPrint(String gub) {
+		// TODO Auto-generated method stub
+		
+		return "HelloWorld : " + gub + "!!!";
+
+	}
+	
+	@RequestMapping("/main31")
+	public String main21() {
+		
+		if(!isChk){
+			isChk = true;
+		}
+		
+		gub = "main31";
+		
+		return CommonUtil.retHellow(gub);
+	}
+	
+	
+}
